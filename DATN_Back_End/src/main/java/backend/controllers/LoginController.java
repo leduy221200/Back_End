1cbe7d0dd40c42cb1086a21b971e329d7d752dbd
leduy221200 <duylpd03134@fpt.edu.vn>
2020-11-08 13:16:50 +0700

    DuyLe: commit code CustomerService.

--- DATN_Back_End/src/main/java/backend/controllers/LoginController.java
+++ DATN_Back_End/src/main/java/backend/controllers/LoginController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import backend.models.Admins;
+import backend.models.Customers;
 import backend.repositories.AdminsServices;
+import backend.repositories.CustomersServices;
 
 @RestController
 public class LoginController {
@@ -17,41 +19,38 @@ public class LoginController {
 	String message = "";
 	@Autowired
 	AdminsServices as;
+	CustomersServices cs;
 	
 	@RequestMapping(value = "/loginAdmin", method = RequestMethod.POST)
-	public String login(@RequestBody Admins model) {
-		
+	public String loginAdmin(@RequestBody Admins model) {	
 		Optional<Admins> checkLogin = as.findById(model.get_idAdmin());
-
 		if (checkLogin.isPresent()) {
 			if (checkLogin.get().getPassWord().matches(model.getPassWord())) {
-				return "/login/admin";
+				return "/adminManager";
 			}else {
 				message = "Password is not correct!";
-				return "/login_fail/admin";
+				return "/loginAdmin";
 			}
 		}else {
 			message = "id is not correct!";
-			return "/login_fail/admin";
+			return "/loginAdmin";
 		}	
 	}
 	
-//	@RequestMapping(value = "/login", method = RequestMethod.POST)
-//	public String login(@RequestBody Admins model) {
-//		String message = "";
-//		Optional<Admins> checkLogin = as.findById(model.get_idAdmin());
-//
-//		if (checkLogin.isPresent()) {
-//			if (checkLogin.get().getPassWord().matches(model.getPassWord())) {
-//				return "/login/admin";
-//			}else {
-//				message = "Password is not correct!";
-//				return "/login_fail/admin";
-//			}
-//		}else {
-//			message = "id is not correct!";
-//			return "/login_fail/admin";
-//		}	
-//	}
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@RequestBody Customers model) {
+		Optional<Customers> checkLogin = cs.findById(model.get_idCustomer());
+		if (checkLogin.isPresent()) {
+			if (checkLogin.get().getPassWord().matches(model.getPassWord())) {
+				return "/index";
+			}else {
+				message = "Password is not correct!";
+				return "/login";
+			}
+		}else {
+			message = "username is not correct!";
+			return "/login";
+		}	
+	}
 	
 }
---
+++ DATN_Back_End/src/main/java/backend/repositories/CustomersServicesImp.java
@@ -0,0 +1,36 @@
+package backend.repositories;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import backend.models.Customers;
+
+@Service
+public class CustomersServicesImp implements CustomersServices{
+	@Autowired
+	CustomersRepository cr;
+
+	@Override
+	public <S extends Customers> S save(S entity) {
+		return cr.save(entity);
+	}
+
+	@Override
+	public Optional<Customers> findById(String id) {
+		return cr.findById(id);
+	}
+
+	@Override
+	public Iterable<Customers> findAll() {
+		return cr.findAll();
+	}
+
+	@Override
+	public void deleteById(String id) {
+		cr.deleteById(id);
+	}
+	
+	
+}
